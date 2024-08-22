e1f9aaed2cd73a53a101777eb0590355e303b682
parththakkar1818 <parththakkar1208@gmail.com>
2024-08-22 12:25:54 +0530

    added event route

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from 'next/server';
 
 import { connectToDB } from '@/lib/connectToDB';
-import User from '@/models/userSchema';
+import Event from '@/models/eventSchema';
 
 export async function POST(req) {
     await connectToDB();
 
     try {
         const data = await req.json();
-        const user = new User(data);
-        await user.save();
-        return NextResponse.json({ success: true, data: user }, { status: 201 });
+        const event = new Event(data);
+        await event.save();
+        return NextResponse.json({ success: true, data: event }, { status: 201 });
     } catch (error) {
         return NextResponse.json({ success: false, error: error.message }, { status: 400 });
     }
@@ -20,9 +20,9 @@ export async function GET(req) {
     await connectToDB();
 
     try {
-        const users = await User.find({});
-        console.log(users);
-        return NextResponse.json({ success: true, data: users }, { status: 200 });
+        const events = await Event.find({});
+        console.log(events);
+        return NextResponse.json({ success: true, data: events }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ success: false, error: error.message }, { status: 400 });
     }
